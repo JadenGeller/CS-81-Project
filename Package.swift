c7a1b9be95cc6b163e4e589954fdb2e79e1fbae7
Jaden Geller <jaden.geller@gmail.com>
2016-02-16 12:47:19 -0800

    Package update

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CS81",
+    name: "ProgrammingLanguage",
     dependencies: [
         .Package(url: "https://github.com/JadenGeller/Axiomatic.git", majorVersion: 1),
         .Package(url: "https://github.com/JadenGeller/Parsley.git", majorVersion: 2)
