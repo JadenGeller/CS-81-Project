cbafe4a688075f06d901c2fcd039148d6452214f
Jaden Geller <jaden.geller@gmail.com>
2016-04-08 14:14:12 -0700

    Added dependencies

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "ProgrammingLanguage",
     dependencies: [
-        .Package(url: "https://github.com/JadenGeller/Axiomatic.git", majorVersion: 1),
-        .Package(url: "https://github.com/JadenGeller/Parsley.git", majorVersion: 2)
+        .Package(url: "https://github.com/JadenGeller/Typist.git", majorVersion: 1),
+        .Package(url: "https://github.com/JadenGeller/Parsley.git", majorVersion: 2),
+        .Package(url: "https://github.com/JadenGeller/Expressive.git", majorVersion: 1)
     ]
 )
